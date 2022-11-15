73238ac630b72a5c90a1debf1244f13cf362ea43
martvanrijthoven <mart.vanrijthoven@gmail.com>
2022-11-15 16:03:23 +0100

    pyyaml req build system

--- .github/scripts/print_version.py
+++ .github/scripts/print_version.py
@@ -1,9 +1,9 @@
-
 from dicfg import __version__
 
 
 def print_version():
-    print('version:', __version__)
+    print("version:", __version__)
+
 
 if __name__ == "__main__":
     print_version()
