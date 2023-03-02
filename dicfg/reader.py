69f9d310cd9784c8606962eaf99f543aba2a8ebc
martvanrijthoven <mart.vanrijthoven@gmail.com>
2023-03-02 16:06:32 +0100

    fix ast literal parsing

--- dicfg/reader.py
+++ dicfg/reader.py
@@ -156,7 +156,11 @@ class ConfigReader:
 def _create_dict_from_keys(keys: list, value) -> dict:
     dictionary = defaultdict(dict)
     if len(keys) <= 1:
-        dictionary[keys[0]] = ast.literal_eval("\'" + value + "\'")
+        try:
+            value = ast.literal_eval(value)
+        except ValueError:
+            value = ast.literal_eval("\'" + value + "\'")
+        dictionary[keys[0]] = value
     else:
         dictionary[keys[0]] = dict(_create_dict_from_keys(keys[1:], value))
     return dict(dictionary)
